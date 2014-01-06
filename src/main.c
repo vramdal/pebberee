2f5972414203c9f5a3680b69314b6acb0b9bce22
vramdal <vramdal@gmail.com>
2014-01-06 12:14:16 -0800

    SDK 2.0

--- src/main.c
+++ src/main.c
@@ -1,43 +1,119 @@
-#include "pebble_os.h"
-#include "pebble_app.h"
-#include "pebble_fonts.h"
+#include "pebble.h"
 
+// This is a custom defined key for saving our count field
+#define NUM_DRINKS_PKEY 1
 
-#define MY_UUID { 0xD5, 0x6B, 0x93, 0x8B, 0x68, 0xE5, 0x47, 0xF0, 0xA9, 0x8E, 0xB0, 0x6B, 0x7D, 0x17, 0x3B, 0xA4 }
-PBL_APP_INFO(MY_UUID,
-             "Pebberee", "vidarramdal.com",
-             1, 0, /* App version */
-             DEFAULT_MENU_ICON,
-             APP_INFO_STANDARD_APP);
+// You can define defaults for values in persistent storage
+#define NUM_DRINKS_DEFAULT 0
 
-Window window;
+static Window *window;
 
+static GBitmap *action_icon_plus;
+static GBitmap *action_icon_minus;
 
-void handle_init(AppContextRef ctx) {
-  (void)ctx;
+static ActionBarLayer *action_bar;
 
-  window_init(&window, "Match");
-  window_stack_push(&window, true /* Animated */);
+static TextLayer *header_text_layer;
+static TextLayer *body_text_layer;
+static TextLayer *label_text_layer;
+
+// We'll save the count in memory from persistent storage
+static int num_drinks = NUM_DRINKS_DEFAULT;
+
+static void update_text() {
+  static char body_text[50];
+  snprintf(body_text, sizeof(body_text), "%u Bottles", num_drinks);
+  text_layer_set_text(body_text_layer, body_text);
 }
 
-static void handle_tick(struct tm *tick_time, TimeUnits units_changed) {
+static void increment_click_handler(ClickRecognizerRef recognizer, void *context) {
+  num_drinks++;
+  update_text();
 }
 
-/*static void handle_battery(BatteryChargeState charge_state) {
-	static char battery_text[] = "100% batteri";
-	if (charge_state.is_charging) {
-		snprint(battery_text, sizeof(battery_text), "lader");
-	} else {
-		snprint(battery_text, sizeof(battery_text), "%d%% batteri", charge_state.charge_percent);
-	}
-	text_layer_set_text(battery_layer, battery_text);
-}*/
+static void decrement_click_handler(ClickRecognizerRef recognizer, void *context) {
+  if (num_drinks <= 0) {
+    // Keep the counter at zero
+    return;
+  }
 
+  num_drinks--;
+  update_text();
+}
 
-void pbl_main(void *params) {
-  PebbleAppHandlers handlers = {
-    .init_handler = &handle_init
-  };
-  tick_timer_service_subscribe(SECOND_UNIT, handle_tick);
-  app_event_loop(params, &handlers);
+static void click_config_provider(void *context) {
+  const uint16_t repeat_interval_ms = 50;
+  window_single_repeating_click_subscribe(BUTTON_ID_UP, repeat_interval_ms, (ClickHandler) increment_click_handler);
+  window_single_repeating_click_subscribe(BUTTON_ID_DOWN, repeat_interval_ms, (ClickHandler) decrement_click_handler);
+}
+
+static void window_load(Window *me) {
+  action_bar = action_bar_layer_create();
+  action_bar_layer_add_to_window(action_bar, me);
+  action_bar_layer_set_click_config_provider(action_bar, click_config_provider);
+
+  action_bar_layer_set_icon(action_bar, BUTTON_ID_UP, action_icon_plus);
+  action_bar_layer_set_icon(action_bar, BUTTON_ID_DOWN, action_icon_minus);
+
+  Layer *layer = window_get_root_layer(me);
+  const int16_t width = layer_get_frame(layer).size.w - ACTION_BAR_WIDTH - 3;
+
+  header_text_layer = text_layer_create(GRect(4, 0, width, 60));
+  text_layer_set_font(header_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+  text_layer_set_background_color(header_text_layer, GColorClear);
+  text_layer_set_text(header_text_layer, "Drink Counter");
+  layer_add_child(layer, text_layer_get_layer(header_text_layer));
+
+  body_text_layer = text_layer_create(GRect(4, 44, width, 60));
+  text_layer_set_font(body_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+  text_layer_set_background_color(body_text_layer, GColorClear);
+  layer_add_child(layer, text_layer_get_layer(body_text_layer));
+
+  label_text_layer = text_layer_create(GRect(4, 44 + 28, width, 60));
+  text_layer_set_font(label_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
+  text_layer_set_background_color(label_text_layer, GColorClear);
+  text_layer_set_text(label_text_layer, "of drinks on the wall");
+  layer_add_child(layer, text_layer_get_layer(label_text_layer));
+
+  update_text();
+}
+
+static void window_unload(Window *window) {
+  text_layer_destroy(header_text_layer);
+  text_layer_destroy(body_text_layer);
+  text_layer_destroy(label_text_layer);
+
+  action_bar_layer_destroy(action_bar);
+}
+
+static void init(void) {
+  action_icon_plus = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_ACTION_ICON_PLUS);
+  action_icon_minus = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_ACTION_ICON_MINUS);
+
+  window = window_create();
+  window_set_window_handlers(window, (WindowHandlers) {
+    .load = window_load,
+    .unload = window_unload,
+  });
+
+  // Get the count from persistent storage for use if it exists, otherwise use the default
+  num_drinks = persist_exists(NUM_DRINKS_PKEY) ? persist_read_int(NUM_DRINKS_PKEY) : NUM_DRINKS_DEFAULT;
+
+  window_stack_push(window, true /* Animated */);
+}
+
+static void deinit(void) {
+  // Save the count into persistent storage on app exit
+  persist_write_int(NUM_DRINKS_PKEY, num_drinks);
+
+  window_destroy(window);
+
+  gbitmap_destroy(action_icon_plus);
+  gbitmap_destroy(action_icon_minus);
+}
+
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
 }
